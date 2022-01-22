3a3275d4b9d5acf9be728af156fb7ca234e5d427
Dioni <eiji.onizuka@gmail.com>
2022-01-22 12:11:22 +0100

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
-import { saludar } from './js/componentes.js';
+export const todoList = new TodoList();
 
-const nombre = 'Dioni';
-saludar( nombre );
+todoList.todos.forEach( crearTodoHtml );
 
-
+console.log( 'todos', todoList.todos );
